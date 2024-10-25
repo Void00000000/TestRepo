eb0f80acfd9538fb4b77dd28cf1f4b23b8f01e1e
Void00000000 <chernikovearl@gmail.com>
2024-10-25 23:24:31 +0700

    Point.cs added

---
+++ GitTestApp/Point.cs
@@ -0,0 +1,12 @@
+﻿namespace GitTestApp
+{
+    internal class Point
+    {
+        public double x;
+        public double y;
+        public override string ToString()
+        {
+            return $"{nameof(x)} = {x}, {nameof(y)} = {y}";
+        }
+    }
+}
--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -1,12 +1,5 @@
-﻿Point2D point1 = new Point2D() { x = 2, y = 2};
-Point2D point2 = new Point2D() { x = 3, y = 3 };
+﻿using GitTestApp;
+
+Point point1 = new Point() { x = 2, y = 2};
+Point point2 = new Point() { x = 3, y = 3 };
 Console.WriteLine(point1.ToString());
-class Point2D
-{
-    public double x;
-    public double y;
-    public override string ToString()
-    {
-        return $"{nameof(x)} = {x}, {nameof(y)} = {y}";
-    }
-}
