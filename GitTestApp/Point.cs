9575b77f9d311b288fcd1d1d29be9f34bf91ec8c
Void00000000 <chernikovearl@gmail.com>
2024-10-25 23:39:32 +0700

    Point class has been changed

--- GitTestApp/Point.cs
+++ GitTestApp/Point.cs
@@ -4,6 +4,7 @@
     {
         public double x;
         public double y;
+        public double z;
         public override string ToString()
         {
             return $"{nameof(x)} = {x}, {nameof(y)} = {y}";
