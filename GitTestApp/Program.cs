595e8d4559df90771b803f6e73f09025ad0a8c76
Void00000000 <chernikovearl@gmail.com>
2024-10-25 23:21:15 +0700

    third commit

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -1,5 +1,6 @@
-﻿Point2D point = new Point2D() { x = 2, y = 2};
-Console.WriteLine(point.ToString());
+﻿Point2D point1 = new Point2D() { x = 2, y = 2};
+Point2D point2 = new Point2D() { x = 3, y = 3 };
+Console.WriteLine(point1.ToString());
 class Point2D
 {
     public double x;
