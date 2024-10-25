86efcd9aceb76919fb93a6cb37eff330cd209664
Void00000000 <chernikovearl@gmail.com>
2024-10-25 23:41:02 +0700

    Program.cs has been changed

--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -1,5 +1,5 @@
 ﻿using GitTestApp;
 
-Point point1 = new Point() { x = 2, y = 2};
-Point point2 = new Point() { x = 3, y = 3 };
+Point point1 = new Point() { x = 2, y = 2, z = 0};
+Point point2 = new Point() { x = 3, y = 3, z = 1 };
 Console.WriteLine(point1.ToString());
