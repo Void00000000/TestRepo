ff7dd440c5df61cab51de80b62143810a09271bd
Void00000000 <chernikovearl@gmail.com>
2024-10-25 22:57:45 +0700

    first commit

---
+++ GitTestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿Point2D point = new Point2D() { x = 2, y = 3};
+Console.WriteLine(point.ToString());
+class Point2D
+{
+    public double x;
+    public double y;
+    public override string ToString()
+    {
+        return $"{nameof(x)} = {x}, {nameof(y)} = {y}";
+    }
+}
