d5898841e0e8a7827627b39c04106f85dcac1ac5
Void00000000 <chernikovearl@gmail.com>
2024-10-27 03:16:37 +0700

    lenght has been added

--- GitTestApp/Point.cs
+++ GitTestApp/Point.cs
@@ -5,6 +5,7 @@
         public double x;
         public double y;
         public double z;
+        public double length;
         public override string ToString()
         {
             return $"Point: {nameof(x)} = {x}, {nameof(y)} = {y} {nameof(y)} = {z}";
--- GitTestApp/Program.cs
+++ GitTestApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using GitTestApp;
 
-Point point1 = new Point() { x = 2, y = 2, z = 0};
-Point point2 = new Point() { x = 3, y = 3, z = 1 };
-Console.WriteLine(point1.ToString());
+Point point = new Point() { x = 2, y = 2, z = 0};
+Console.WriteLine(point.ToString());
