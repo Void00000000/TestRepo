ec9f45c6b4d85c8de398ab974a584d516d8263aa
Void00000000 <chernikovearl@gmail.com>
2024-10-27 03:50:50 +0700

    merge conflict resolved

--- GitTestApp/Point.cs
+++ GitTestApp/Point.cs
@@ -5,10 +5,11 @@
         public double x;
         public double y;
         public double z;
+        public double w;
         public double length;
         public override string ToString()
         {
-            return $"Point: {nameof(x)} = {x}, {nameof(y)} = {y} {nameof(y)} = {z}";
+            return $"Point: {nameof(x)} = {x}, {nameof(y)} = {y} {nameof(y)} = {z} {nameof(w)} = {w}";
         }
     }
 }
